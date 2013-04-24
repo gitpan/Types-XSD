b9c371f52d7951045f3241d3b28bfb40509756f5
Toby Inkster <tobyink@cpan.org>
2013-04-24 21:10:54 +0000

    Import of TOBYINK/Types-XSD-0.000_03 from CPAN.
    gitpan-cpan-distribution: Types-XSD
    gitpan-cpan-version:      0.000_03
    gitpan-cpan-path:         TOBYINK/Types-XSD-0.000_03.tar.gz
    gitpan-cpan-author:       TOBYINK
    gitpan-cpan-maturity:     developer

--- lib/Types/XSD.pm
+++ lib/Types/XSD.pm
@@ -7,7 +7,7 @@ use utf8;
 
 BEGIN {
 	$Types::XSD::AUTHORITY = 'cpan:TOBYINK';
-	$Types::XSD::VERSION   = '0.000_02';
+	$Types::XSD::VERSION   = '0.000_03';
 }
 
 use B qw(perlstring);
@@ -105,8 +105,41 @@ sub b64_length
 	(length($str) * 3 / 4) - $padding;
 }
 
-our @patterns; my $pattern_i = -1;
+our @patterns;   my $pattern_i = -1;
+our @assertions; my $assertion_i = -1;
 my %facets = (
+	assertions => sub {
+		my ($o, $var) = @_;
+		return unless exists $o->{assertions};
+		my $ass = delete $o->{assertions};
+		$ass = [$ass] unless ref($ass) eq q(ARRAY);
+		my @r;
+		for my $a (@$ass)
+		{
+			require Types::TypeTiny;
+			if (Types::TypeTiny::CodeLike()->check($a))
+			{
+				$assertion_i++;
+				$assertions[$assertion_i] = $a;
+				push @r,
+					($var eq '$_')
+						? sprintf('$Types::XSD::assertions[%d]->(%s)', $assertion_i, $var)
+						: sprintf('do { local $_ = %s; $Types::XSD::assertions[%d]->(%s) }', $var, $assertion_i, $var);
+			}
+			elsif (Types::TypeTiny::StringLike()->check($a))
+			{
+				push @r,
+					($var eq '$_')
+						? "do { $a }"
+						: "do { local \$_ = $var; $a }";
+			}
+			else
+			{
+				croak "assertions should be strings or coderefs";
+			}
+		}
+		join ' && ', map "($_)", @r;
+	},
 	length => sub {
 		my ($o, $var) = @_;
 		return unless exists $o->{length};
@@ -187,6 +220,18 @@ my %facets = (
 		return unless exists $o->{whiteSpace};
 		delete($o->{whiteSpace});
 		"!!1";
+	},
+	explicitTimezone => sub {
+		my ($o, $var) = @_;
+		return unless exists $o->{explicitTimezone};
+		my $etz = delete $o->{explicitTimezone};
+		return sprintf('%s =~ m/(?:Z|(?:[+-]\d{2}:?\d{2}))$/xism', $var)
+			if lc($etz) eq 'required';
+		return sprintf('%s !~ m/(?:Z|(?:[+-]\d{2}:?\d{2}))$/xism', $var)
+			if lc($etz) eq 'prohibited';
+		return '!!1'
+			if lc($etz) eq 'optional';
+		croak "explicitTimezone facet expected to be 'required', 'prohibited' or 'optional'"
 	},
 	maxInclusive => sub {
 		my ($o, $var) = @_;
@@ -303,7 +348,7 @@ my %facets = (
 sub facet
 {
 	my $self   = pop;
-	my @facets = @_;
+	my @facets = ("assertions", @_);
 	my $regexp = qr{^${\(join "|", map quotemeta, @facets)}$}ms;
 	my $name   = "$self";
 	
@@ -364,15 +409,15 @@ sub dt_maker
 	{
 		my $var = $_[1];
 		my @code;
-		push @code, "do {";
+		push @code, "do { my \$ok = 1;";
 		push @code, sprintf(
-			'my (%s) = (%s =~ $Types::XSD::dtarr[%d]);',
+			'my (%s) = (%s =~ $Types::XSD::dtarr[%d]) or --$ok;',
 			join(', ', map "\$$_", @fields),
 			$var,
 			$j,
 		);
 		push @code, sprintf(
-			'eval { "DateTime::Incomplete"->new(%s)->to_datetime(base => $Types::XSD::base_datetime) };',
+			'$ok and eval { "DateTime::Incomplete"->new(%s)->to_datetime(base => $Types::XSD::base_datetime) };',
 			join(', ', map "$_ => \$$_", @fields),
 		);
 		push @code, "}";
@@ -388,7 +433,7 @@ sub dt_maker
 	__PACKAGE__->add_type($type);
 	
 	facet(
-		qw( pattern whiteSpace enumeration maxInclusiveDT maxExclusiveDT minInclusiveDT minExclusiveDT ),
+		qw( pattern whiteSpace enumeration maxInclusiveDT maxExclusiveDT minInclusiveDT minExclusiveDT explicitTimezone ),
 		$type,
 	);
 	
@@ -432,6 +477,7 @@ use Type::Library -base, -declare => qw(
 	NonNegativeInteger PositiveInteger UnsignedLong UnsignedInt
 	UnsignedShort UnsignedByte Duration DateTime Time Date GYearMonth
 	GYear GMonthDay GDay GMonth
+	DateTimeStamp YearMonthDuration DayTimeDuration
 );
 
 our @EXPORT_OK = qw( dt_cmp dur_cmp dt_parse dur_parse );
@@ -564,6 +610,12 @@ declare Duration, as Types::Standard::StrMatch[
 	$}xism
 ];
 
+facet qw( pattern whiteSpace enumeration maxInclusiveDuration maxExclusiveDuration minInclusiveDuration minExclusiveDuration ),
+declare YearMonthDuration, as Duration->parameterize(pattern => qr{^[^DT]*$});
+
+facet qw( pattern whiteSpace enumeration maxInclusiveDuration maxExclusiveDuration minInclusiveDuration minExclusiveDuration ),
+declare DayTimeDuration, as Duration->parameterize(pattern => qr{^[^YM]*[DT].*$});
+
 dt_maker(
 	DateTime => qr{^
 		(-?[0-9]{4,})
@@ -578,6 +630,24 @@ dt_maker(
 		:
 		([0-9]{2}(?:\.[0-9]+)?)
 		(Z | (?: [+-]\d{2}:?\d{2} ))?
+	$}xism,
+	qw( year month day hour minute second time_zone ),
+);
+
+dt_maker(
+	DateTimeStamp => qr{^
+		(-?[0-9]{4,})
+		-
+		([0-9]{2})
+		-
+		([0-9]{2})
+		T
+		([0-9]{2})
+		:
+		([0-9]{2})
+		:
+		([0-9]{2}(?:\.[0-9]+)?)
+		(Z | (?: [+-]\d{2}:?\d{2} ))
 	$}xism,
 	qw( year month day hour minute second time_zone ),
 );
@@ -681,6 +751,11 @@ Types::XSD - type constraints based on XML schema datatypes
 
 =head1 DESCRIPTION
 
+Types::XSD is a type constraint library inspired by XML Schema, and built
+with L<Type::Library>. It can be used as a type constraint library for
+L<Moo>, L<Mouse> or L<Moose>, or used completely independently of any OO
+framework.
+
 =head2 Type Constraints
 
 This module defines the following type constraints based on the data types
@@ -688,101 +763,236 @@ defined in L<XML Schema|http://www.w3.org/TR/xmlschema-2/>. (The names of
 the type constraints are the same as the XML Schema data types, but
 capitalization often differs.)
 
+I've added some quick explainations of what each type is, but for details,
+see the XML Schema specification.
+
 =over
 
 =item C<< AnyType >>
 
+As per C<Any> from L<Types::Standard>.
+
 =item C<< AnySimpleType >>
+
+As per C<Value> from L<Types::Standard>.
 
 =item C<< String >>
 
+As per C<Str> from L<Types::Standard>.
+
 =item C<< NormalizedString >>
+
+A string containing no line breaks, carriage returns or tabs.
 
 =item C<< Token >>
 
+Like C<NormalizedString>, but also no leading or trailing space, and no
+doubled spaces (i.e. not C<< /\s{2,}/ >>).
+
 =item C<< Language >>
+
+An RFC 3066 language code.
 
 =item C<< Name >>
 
+Something that could be a valid XML element or attribute name. These roughly
+correspond to Perl identifiers but may also contain colons, hyphens and stops.
+(Digits, hyphens and stops are not allowed as the first character.)
+
 =item C<< NmToken >>
+
+Slightly looser version of C<Name>; allows digits, hyphens and stops in the
+first character.
 
 =item C<< NmTokens >>
 
+Space-separated list of C<NmToken>.
+
 =item C<< NCName >>
+
+Slightly tighter vesion of C<Name>; disallows colons.
 
 =item C<< Id >>
 
+Effectively the same as C<NCName>.
+
 =item C<< IdRef >>
+
+Effectively the same as C<NCName>.
 
 =item C<< IdRefs >>
 
+Space-separated list of C<IdRef>.
+
 =item C<< Entity >>
+
+Effectively the same as C<NCName>.
 
 =item C<< Entities >>
 
+Space-separated list of C<Entity>.
+
 =item C<< Boolean >>
+
+Allows C<< "true" >>, C<< "false" >>, C<< "1" >> and C<< "0" >>
+(case-insensitively).
+
+Gotcha: The string C<< "false" >> evaluates to true in Perl. You probably
+want to use C<< Bool >> from L<Types::Standard> instead.
 
 =item C<< Base64Binary >>
 
+Strings which are valid Base64 data. Allows whitespace.
+
+Gotcha: If you parameterize this with C<length>, C<maxLength> or C<minLength>,
+it is the length of the I<decoded> string which will be checked.
+
 =item C<< HexBinary >>
+
+Strings which are valid hexadecimal data. Disallows whitespace; disallows
+leading C<< 0x >>.
+
+Gotcha: If you parameterize this with C<length>, C<maxLength> or C<minLength>,
+it is the length of the I<decoded> string which will be checked.
 
 =item C<< Float >>
 
+As per C<Num> from L<Types::Standard>.
+
 =item C<< Double >>
+
+As per C<Num> from L<Types::Standard>.
 
 =item C<< AnyURI >>
 
+Any absolute I<< or relative >> URI. Effectively, any string at all!
+
 =item C<< QName >>
+
+An XML QName; something that could be used as a valid element name in a
+namespaced XML document.
+
+Gotcha: while C<length>, C<maxLength> and C<minLength> are allowed facets for
+parameterization, they are silently ignored, as per the specification!
 
 =item C<< Notation >>
 
+Effectively the same as C<QName>. According to XML Schema, this is I<always>
+supposed to be parameterized with an enumeration. But we don't enforce that.
+
+Gotcha: while C<length>, C<maxLength> and C<minLength> are allowed facets for
+parameterization, they are silently ignored, as per the specification!
+
 =item C<< Decimal >>
+
+Numbers possibly including a decimal point, but not allowing exponential
+notation (e.g. C<< "3.14e-3" >>).
 
 =item C<< Integer >>
 
+As per C<Int> from L<Types::Standard>.
+
 =item C<< NonPositiveInteger >>
+
+An C<Integer> 0 or below.
 
 =item C<< NegativeInteger >>
 
+An C<Integer> -1 or below.
+
 =item C<< Long >>
+
+An C<Integer> between -9223372036854775808 and 9223372036854775807 (inclusive).
 
 =item C<< Int >>
 
+An C<Integer> between -2147483648 and 2147483647 (inclusive).
+
 =item C<< Short >>
+
+An C<Integer> between -32768 and 32767 (inclusive).
 
 =item C<< Byte >>
 
+An C<Integer> between -128 and 127 (inclusive).
+
 =item C<< NonNegativeInteger >>
+
+An C<Integer> 0 or above.
 
 =item C<< PositiveInteger >>
 
+An C<Integer> 1 or above.
+
 =item C<< UnsignedLong >>
+
+A C<NonNegativeInteger> between 0 and 18446744073709551615 (inclusive).
 
 =item C<< UnsignedInt >>
 
+A C<NonNegativeInteger> between 0 and 4294967295 (inclusive).
+
 =item C<< UnsignedShort >>
+
+A C<NonNegativeInteger> between 0 and 65535 (inclusive).
 
 =item C<< UnsignedByte >>
 
+A C<NonNegativeInteger> between 0 and 255 (inclusive).
+
 =item C<< Duration >>
+
+An ISO 8601 duration.
+
+=item C<< YearMonthDuration >>
+
+An ISO 8601 duration restricted to cover only years and months.
+
+=item C<< DayTimeDuration >>
+
+An ISO 8601 duration restricted to cover only days, hours, minutes and
+seconds. (Note that this still permits durations of many years, as the
+days component is an arbitrary non-negative integer.)
 
 =item C<< DateTime >>
 
+An ISO 8601 datetime with optional timezone.
+
+=item C<< DateTimeStamp >>
+
+An ISO 8601 datetime with required timezone.
+
 =item C<< Time >>
+
+An ISO 8601 time with optional timezone.
 
 =item C<< Date >>
 
+An ISO 8601 date with optional timezone.
+
 =item C<< GYearMonth >>
+
+An year-month pair with optional timezone.
 
 =item C<< GYear >>
 
+An year with optional timezone.
+
 =item C<< GMonthDay >>
+
+An month-day pair with optional timezone.
 
 =item C<< GDay >>
 
+An day with optional timezone.
+
 =item C<< GMonth >>
 
+An month pair with optional timezone.
+
 =back
+
+=head2 Parameters
 
 Datatypes can be parameterized using the facets defined by XML Schema. For
 example:
@@ -796,13 +1006,86 @@ example:
    has rating => (is => "ro", isa => PositiveInteger[ maxInclusive => 5 ]);
    has size   => (is => "ro", isa => Token[ enumeration => \@sizes ]);
 
+The following facets exist, but not all facets are supported for all
+datatypes. (The module will croak if you try to use an unsupported facet.)
+
+=over
+
+=item C<< enumeration >>
+
+An arrayref of allowable values. You should probably use L<Type::Tiny::Enum>
+instead.
+
+=item C<< pattern >>
+
+A regular expression that the value is expected to conform to. Use a normal
+Perl quoted regexp:
+
+   Token[ pattern => qr{^[a-z]+$} ]
+
+=item C<< whiteSpace >>
+
 The C<whiteSpace> facet is ignored as I'm not entirely sure what it should
 do. It perhaps makes sense for coercions, but this module doesn't define any
 coercions.
 
+=item C<< assertions >>
+
+An arrayref of arbitrary additional restrictions, expressed as strings of
+Perl code or coderefs operating on C<< $_ >>.
+
+For example:
+
+   Integer[
+      assertions => [
+         '$_ % 3 == 0',            # multiple of three, and...
+         sub { is_nice($_) },      # is nice (whatever that means)
+      ],
+   ],
+
+Strings of Perl code will result in faster-running type constraints.
+
+=item C<< length >>, C<< maxLength >>, C<< minLength >>
+
+Restrict the length of a value. For example C<< Integer[length=>2] >> allows
+C<10>, C<99> and C<-1>, but not C<100>, C<9> or C<-10>.
+
+Types::XSD won't prevent you from making ridiculous constraints such as
+C<< String[ maxLength => 1, minLength => 2 ] >>.
+
+Note that on C<HexBinary> and C<Base64Binary> types, the lengths apply to
+the decoded string. Length restrictions are silently ignored for C<QName>
+and C<Notation> because the W3C doesn't think you should care what length
+these datatypes are.
+
+=item C<< maxInclusive >>, C<< minInclusive >>, C<< maxExclusive >>, C<< minExclusive >>
+
+Supported for numeric types and datetime/duration-related types.
+
 Note that to be super-correct, the C<< {max,min}{Inclusive,Exclusive} >>
 facets for numeric types are performed by passing the numbers through
 L<Math::BigInt> or L<Math::BigFloat>, so may be a little slow.
+
+=item C<< totalDigits >>
+
+For a decimal (or type derived from decimals) specifies that the total number
+of digits for the value must be at most this number. Given
+C<< Decimal[ totalDigits => 3 ] >>, C<1.23>, C<12.3>, C<123>, C<1.2> and C<1>
+are all allowable; C<1.234> is not. C<1.230> is also not, but this may change
+in a future version.
+
+=item C<< fractionDigits >>
+
+Like C<totalDigits> but ignores digits before the decimal point.
+
+=item C<< explicitTimezone >>
+
+May be C<< "optional" >>, C<< "prohibited" >> or C<< "required" >>. For
+example:
+
+   Time[ explicitTimezone => "prohibited" ]
+
+=back
 
 =head2 Functions
 
@@ -850,6 +1133,18 @@ L<http://rt.cpan.org/Dist/Display.html?Queue=Types-XSD>.
 =head1 SEE ALSO
 
 L<Type::Tiny>, L<Types::Standard>.
+
+=over
+
+=item *
+
+L<http://www.w3.org/TR/xmlschema-2/> Datatypes in XML Schema 1.0
+
+=item *
+
+L<http://www.w3.org/TR/xmlschema11-2/> Datatypes in XML Schema 1.1
+
+=back
 
 =head1 AUTHOR
 
